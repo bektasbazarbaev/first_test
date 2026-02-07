4b0140272bdd9bc7699cc179a3eda3d8352392cf
bektasbazarbaev <bektasbazarbaev9@gmail.com>
2026-02-07 07:56:07 +0500

    Add Practice 2 _ pyhthon lab2 and flow examples

---
+++ practicee 2/lab2/219.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+doramas = {}
+
+for _ in range(n):
+    name, k = input().split()
+    k = int(k)
+    if name in doramas:
+        doramas[name] += k
+    else:
+        doramas[name] = k
+
+for name in sorted(doramas):
+    print(name, doramas[name])
